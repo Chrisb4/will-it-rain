5a14fefdeeda93030c31190bc2d206f878a95128
Chris Brown <nudgechris@gmail.com>
2018-08-30 00:30:35 -0500

    added forecast route

--- routes/index.js
+++ routes/index.js
@@ -14,25 +14,47 @@ router.get('/', (req, res) => {
 router.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  // let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
   request(url, function(err, response, body) {
     if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+      res.render('index', {title: 'will it rain1?',weather: null, error: 'Error, please try again'});
     } else {
       let weather = JSON.parse(body);
       if(weather.main == undefined){
         console.log(weather);
-        res.render('index', {weather: null, error: 'Error, please try again, weather.main is null'});
+        res.render('index', {title: 'will it rain2?', weather: null, error: 'Error, please try again, weather.main is null'});
       } else {
         console.log(weather);
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}`;
         res.render('index', { title: 'will it rain?', weather: weatherText, error: null});
       }
     }
-  })
+  });
+});
 
+router.post('/forecast', function (req, res) {
+  let city = req.body.city;
+  // let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
-  
+  request(url, function(err, response, body) {
+    if(err){
+      res.render('index', {title: 'will it rain1?',weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      if(!weather.main == undefined){
+        console.log(weather);
+        res.render('index', {title: 'will it rain2?', weather: null, error: 'Error, please try again, weather.main is null'});
+      } else {
+        console.log(weather);
+        // let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}`;
+        let weatherText = 'weatherText'
+        res.render('forecast', { title: 'will it rain?', weather: weatherText, error: null, data: weather});
+        // res.json(weather);
+      }
+    }
+  });
 });
 
 module.exports = router;
